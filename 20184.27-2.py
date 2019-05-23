9cf88f960cee2514ea723debec8b8632f582d0de
STARTOVER <37795926+lixiangheng@users.noreply.github.com>
2019-05-23 17:44:09 +0800

    Update 20184.27-2.py

--- 20184.27-2.py
+++ 20184.27-2.py
@@ -8,15 +8,10 @@
 2、第一个输入名字后，屏幕输出信息，并将名字和数字记录到文件里，程序不能退出继续等待别的学生输入。'''
 
 
-
 import random
 
 # 获取随机数字1-99不重复
-def suiji():
-    a = 1
-    b = 99
-    result = random.sample(range(a, b+1), 1)
-    return result
+
 
 dit = {}
 
@@ -24,11 +19,18 @@ while True:
     name = input("输入你的名字的拼音：")
     if name == 'quit':
         break
-    dit[name] = (suiji())
+    if name.isspace() or len(name) == 0:
+        print("不能为空，请重新输入！")
+        name = input("输入你的名字的拼音：")
+
+    dit[name] = (random.sample(range(1, 99+1), 1))
 
 
 # 词典排序，生成新的词典
 dit2 = sorted(dit.items(), key=lambda x: x[1], reverse=True)
 
 
-print(dit2[:3])
+print(dit2)
+
+for to in dit2[:3]:
+    print("姓名: %s, 随机号%s" % (to[0], to[1]))
